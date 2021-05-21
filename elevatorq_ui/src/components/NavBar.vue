d810bd424aa6ec42d77ef4067e17d5707961fa10
Emmanuel Sandorfi <c@parleweb.fr>
2021-05-21 12:37:27 +0200

    code: add the api and update the front

--- elevatorq_ui/src/components/NavBar.vue
+++ elevatorq_ui/src/components/NavBar.vue
@@ -6,12 +6,22 @@
 		</a>
 		<div class="d-flex">
 			<div class="form-check form-switch me-5 d-flex align-items-center">
-				<input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked>
-				<label class="form-check-label" for="flexSwitchCheckDefault" >&nbsp;system on</label>
+				<input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" disabled>
+				<label class="form-check-label" for="flexSwitchCheckDefault" >&nbsp;system on/off</label>
 			</div>
 			
-			<a href="/admin" class="btn btn-outline-primary">admin</a>
-			<a href="https://gitlab.com/esandorfi/elevatorq/-/tree/master/docs/iter3" class="btn btn-outline-primary">help</a>
+			<a href="/admin" class="btn btn-dark me-2">admin</a>
+			<div class="dropdown me-2">
+				<button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+					api
+				</button>
+				<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+					<li><a class="dropdown-item" href="/api/building/">api building</a></li>
+					<li><a class="dropdown-item" href="/api/elevatorq/">api elevatorq</a></li>
+					<li><a class="dropdown-item" href="/api/pressbtnq/">api pressbtnq</a></li>
+				</ul>
+			</div>			
+			<a href="https://gitlab.com/esandorfi/elevatorq/-/tree/master/docs/iter3" class="btn btn-outline-dark me-5">readme</a>
 		</div>
 	</div>
 </nav>
